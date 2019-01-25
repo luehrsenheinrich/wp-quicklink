4cb5f87acb93c5100aad5767c8994ae91a34b02a
Hendrik Luehrsen <hendrik@luehrsen.de>
2019-01-25 20:22:24 +0100

    Validate defaults a bit more

--- build/js/quicklink.js
+++ build/js/quicklink.js
@@ -2,24 +2,49 @@ import quicklink from "quicklink/dist/quicklink.mjs";
 
 document.addEventListener( "DOMContentLoaded", () => {
 
-	const options = Object.assign( {}, window.quicklinkOptions );
+	const tempOptions = Object.assign( {}, window.quicklinkOptions );
+	const options = {};
+
 
 	/* Convert selector into element reference. */
-	if ( options.el ) {
-		options.el = document.querySelector( options.el );
+	if ( tempOptions.el && tempOptions.el !== '' ) {
+		options.el = document.querySelector( tempOptions.el );
+	}
+
+	/* Verify we don't get an empty array, as that would turn off quicklink */
+	if ( 	tempOptions.urls &&
+				typeof tempOptions.urls === 'object' &&
+				tempOptions.urls.length > 0
+			) {
+		options.urls = tempOptions.urls;
+	}
+
+	/* Verify we actually get an int for ms */
+ 	if ( parseInt( tempOptions.timeout ) ) {
+		options.timeout = parseInt( tempOptions.timeout );
+	}
+
+	/* Obtain function reference. */
+	if ( tempOptions.timeoutFn && 'requestIdleCallback' !== tempOptions.timeoutFn ) {
+		options.timeoutFn = window[ tempOptions.timeoutFn ];
+	}
+
+	/* Verify we don't get an empty array, as that would turn off quicklink */
+	if ( 	tempOptions.origins &&
+				typeof tempOptions.origins === 'object' &&
+				tempOptions.origins.length > 0
+			) {
+		options.origins = tempOptions.origins;
 	}
 
 	/* Convert strings to regular expressions. */
-	if ( options.ignores ) {
-		options.ignores = options.ignores.map( ( ignore ) => {
+	if ( tempOptions.ignores ) {
+		options.ignores = tempOptions.ignores.map( ( ignore ) => {
 			return new RegExp( ignore );
 		} );
 	}
 
-	/* Obtain function reference. */
-	if ( options.timeoutFn && 'requestIdleCallback' !== options.timeoutFn ) {
-		options.timeoutFn = window[ options.timeoutFn ];
-	}
+	console.log( options );
 
 	quicklink( options );
 } );
