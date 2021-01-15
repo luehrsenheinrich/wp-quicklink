e1ce7df81100b964332b1fa39ec518c69570e4b9
Hendrik Luehrsen <hendrik@luehrsen.de>
2021-01-15 13:34:01 +0100

    Update the options formatting
    Account for the two new options (limit, throttle) and move the URLs option to the new `prefetch` function.

--- build/js/quicklink.js
+++ build/js/quicklink.js
@@ -1,49 +1,59 @@
-import { listen } from 'quicklink';
+import { listen, prefetch } from 'quicklink';
 
 window.addEventListener( 'load', () => {
 	const exportedOptions = window.quicklinkOptions || {};
 
-	const options = {};
+	const listenerOptions = {};
 
 	// el: Convert selector into element reference.
 	if ( 'string' === typeof exportedOptions.el && exportedOptions.el ) {
-		options.el = document.querySelector( exportedOptions.el );
-	}
-
-	// urls: Verify we don't get an empty array, as that would turn off quicklink.
-	if ( Array.isArray( exportedOptions.urls ) && 0 < exportedOptions.urls.length ) {
-		options.urls = exportedOptions.urls;
+		listenerOptions.el = document.querySelector( exportedOptions.el );
 	}
 
 	// timeout: Verify we actually get an int for milliseconds.
 	if ( 'number' === typeof exportedOptions.timeout ) {
-		options.timeout = exportedOptions.timeout;
+		listenerOptions.timeout = exportedOptions.timeout;
+	}
+
+	// limit: Verify we actually get an int.
+	if ( 'number' === typeof exportedOptions.limit ) {
+		listenerOptions.limit = exportedOptions.limit;
+	}
+
+	// throttle: Verify we actually get an int.
+	if ( 'number' === typeof exportedOptions.throttle ) {
+		listenerOptions.throttle = exportedOptions.throttle;
 	}
 
 	// timeoutFn: Obtain function reference as opposed to function string, if it is not the default.
 	if ( 'string' === typeof exportedOptions.timeoutFn && 'requestIdleCallback' !== exportedOptions.timeoutFn ) {
 		const timeoutFn = window[ exportedOptions.timeoutFn ];
-		options.timeoutFn = function() {
+		listenerOptions.timeoutFn = function() {
 			return timeoutFn.apply( window, arguments );
 		};
 	}
 
 	// priority: Obtain priority.
 	if ( 'boolean' === typeof exportedOptions.priority ) {
-		options.priority = exportedOptions.priority;
+		listenerOptions.priority = exportedOptions.priority;
 	}
 
 	// origins: Verify we don't get an empty array, as that would turn off quicklink.
 	if ( Array.isArray( exportedOptions.origins ) && 0 < exportedOptions.origins.length ) {
-		options.origins = exportedOptions.origins;
+		listenerOptions.origins = exportedOptions.origins;
 	}
 
 	// ignores: Convert strings to regular expressions.
 	if ( Array.isArray( exportedOptions.ignores ) && 0 < exportedOptions.ignores.length ) {
-		options.ignores = exportedOptions.ignores.map( ( ignore ) => {
+		listenerOptions.ignores = exportedOptions.ignores.map( ( ignore ) => {
 			return new RegExp( ignore );
 		} );
 	}
 
-	listen( options );
+	listen( listenerOptions );
+
+	// urls: Verify we don't get an empty array, as that would turn off quicklink.
+	if ( Array.isArray( exportedOptions.urls ) && 0 < exportedOptions.urls.length ) {
+		prefetch( exportedOptions.urls );
+	}
 } );
