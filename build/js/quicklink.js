38ea04660536439f7a5173b2d5ceee7928c41ee0
Hendrik Luehrsen <hendrik@luehrsen.de>
2021-08-16 18:40:52 +0200

    Adhere to new eslint rules

--- build/js/quicklink.js
+++ build/js/quicklink.js
@@ -3,7 +3,7 @@ import * as quicklink from 'quicklink';
 // Move quicklink to the global scope
 window.quicklink = quicklink;
 
-window.addEventListener( 'load', () => {
+global.addEventListener( 'load', () => {
 	const exportedOptions = window.quicklinkOptions || {};
 
 	const listenerOptions = {};
@@ -19,17 +19,27 @@ window.addEventListener( 'load', () => {
 	}
 
 	// limit: Verify we actually get an int.
-	if ( 'number' === typeof exportedOptions.limit && exportedOptions.limit > 0 ) {
+	if (
+		'number' === typeof exportedOptions.limit &&
+		exportedOptions.limit > 0
+	) {
 		listenerOptions.limit = exportedOptions.limit;
 	}
 
 	// throttle: Verify we actually get an int.
-	if ( 'number' === typeof exportedOptions.throttle && exportedOptions.throttle > 0 ) {
+	if (
+		'number' === typeof exportedOptions.throttle &&
+		exportedOptions.throttle > 0
+	) {
 		listenerOptions.throttle = exportedOptions.throttle;
 	}
 
 	// timeoutFn: Obtain function reference as opposed to function string, if it is not the default.
-	if ( 'string' === typeof exportedOptions.timeoutFn && 'requestIdleCallback' !== exportedOptions.timeoutFn && typeof 'function' === window[ exportedOptions.timeoutFn ] ) {
+	if (
+		'string' === typeof exportedOptions.timeoutFn &&
+		'requestIdleCallback' !== exportedOptions.timeoutFn &&
+		typeof 'function' === window[ exportedOptions.timeoutFn ]
+	) {
 		const timeoutFn = window[ exportedOptions.timeoutFn ];
 		listenerOptions.timeoutFn = function() {
 			return timeoutFn.apply( window, arguments );
@@ -37,7 +47,10 @@ window.addEventListener( 'load', () => {
 	}
 
 	// onError: Obtain function reference as opposed to function string, if it is not the default.
-	if ( 'string' === typeof exportedOptions.onError && typeof 'function' === window[ exportedOptions.onError ] ) {
+	if (
+		'string' === typeof exportedOptions.onError &&
+		typeof 'function' === window[ exportedOptions.onError ]
+	) {
 		const onError = window[ exportedOptions.onError ];
 		listenerOptions.onError = function() {
 			return onError.apply( window, arguments );
@@ -50,12 +63,18 @@ window.addEventListener( 'load', () => {
 	}
 
 	// origins: Verify we don't get an empty array, as that would turn off quicklink.
-	if ( Array.isArray( exportedOptions.origins ) && 0 < exportedOptions.origins.length ) {
+	if (
+		Array.isArray( exportedOptions.origins ) &&
+		0 < exportedOptions.origins.length
+	) {
 		listenerOptions.origins = exportedOptions.origins;
 	}
 
 	// ignores: Convert strings to regular expressions.
-	if ( Array.isArray( exportedOptions.ignores ) && 0 < exportedOptions.ignores.length ) {
+	if (
+		Array.isArray( exportedOptions.ignores ) &&
+		0 < exportedOptions.ignores.length
+	) {
 		listenerOptions.ignores = exportedOptions.ignores.map( ( ignore ) => {
 			return new RegExp( ignore );
 		} );
@@ -66,7 +85,10 @@ window.addEventListener( 'load', () => {
 	/**
 	 * The option to prefetch urls from the options is deprecated as of version 0.8.0.
 	 */
-	if ( Array.isArray( exportedOptions.urls ) && 0 < exportedOptions.urls.length ) {
+	if (
+		Array.isArray( exportedOptions.urls ) &&
+		0 < exportedOptions.urls.length
+	) {
 		quicklink.prefetch( exportedOptions.urls );
 	}
 } );
