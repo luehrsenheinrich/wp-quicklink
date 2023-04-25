f900a4fdcec95cc567409d57c04e86f385f01fb5
Hendrik Luehrsen <hendrik@luehrsen.de>
2023-04-25 10:30:43 +0200

    Replace UglifyJS with Terser

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,42 @@
-const path = require('path');
-const webpack = require('webpack');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const path = require( 'path' );
+const webpack = require( 'webpack' );
+const TerserPlugin = require( 'terser-webpack-plugin' );
 
 module.exports = {
-  entry: {
-    'quicklink.min': path.resolve(__dirname, './build/js/quicklink.js'),
-		'quicklink.bundle': path.resolve(__dirname, './build/js/quicklink.js'),
-  },
-  mode: 'none',
-  output: {
-    path: path.resolve(__dirname, './build'),
-    filename: '[name].js'
-  },
-  plugins: [
-		new webpack.ProvidePlugin({
-			jQuery: "jquery",
-			wp: 'wp'
-		}),
-	  new UglifyJsPlugin({
-			include: /\.min\.js$/,
-    })
-  ],
-  externals: {
+	entry: {
+		'quicklink.min': path.resolve( __dirname, './build/js/quicklink.js' ),
+		'quicklink.bundle': path.resolve( __dirname, './build/js/quicklink.js' ),
+	},
+	mode: 'production',
+	optimization: {
+		minimizer: [
+			new TerserPlugin( {
+				include: /\.min\.js$/,
+			} ),
+		],
+	},
+	output: {
+		path: path.resolve( __dirname, './build' ),
+		filename: '[name].js',
+	},
+	plugins: [
+		new webpack.ProvidePlugin( {
+			jQuery: 'jquery',
+			wp: 'wp',
+		} ),
+	],
+	externals: {
 		jquery: 'jQuery',
-		wp: 'wp'
-  },
-  resolve: {
+		wp: 'wp',
+	},
+	resolve: {
 		modules: [
 			'node_modules',
 		],
 	},
 	module: {
-	  rules: [
-	    { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader", include: path.resolve(__dirname, "build") }
-	  ]
-	}
+		rules: [
+			{ test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader', include: path.resolve( __dirname, 'build' ) },
+		],
+	},
 };
